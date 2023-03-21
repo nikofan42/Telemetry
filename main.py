862033c6577b0988a383a136b6ca6f6a87e87080
niko <blankkeni@gmail.com>
2023-03-21 16:34:34 +0200

    wowzers

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class State:
     last_car_setup_tick = -1
     lap_counter = 0
     previousFuelLevel = 0
+    in_startup = 1
+    pit_flag = 0
 
 
 # here we check if we are connected to iracing
@@ -27,6 +29,8 @@ def check_iracing():
 
 # our main loop, where we retrieve data
 # and do something useful with it
+
+
 def loop():
     # on each tick we freeze buffer with live telemetry
     # it is optional, but useful if you use vars like CarIdxXXX
@@ -37,80 +41,58 @@ def loop():
     # and you will get incosistent data
     ir.freeze_var_buffer_latest()
 
-    # retrieve live telemetry data
-    # check here for list of available variables
-    # https://github.com/kutu/pyirsdk/blob/master/vars.txt
-    # this is not full list, because some cars has additional
-    # specific variables, like break bias, wings adjustment, etc
-    t = ir['SessionTime']
-    rpm = ir['RPM']
-    # print('session time:', t)
-    # print('rpm: ', rpm)
+    if state.in_startup == 1:
+        state.in_startup = 0
+        state.previousFuelLevel = ir['FuelLevel']
 
-    # retrieve CarSetup from session data
-    # we also check if CarSetup data has been updated
-    # with ir.get_session_info_update_by_key(key)
-    # but first you need to request data, before checking if its updated
-    car_setup = ir['CarSetup']
-    if car_setup:
-        car_setup_tick = ir.get_session_info_update_by_key('CarSetup')
-        if car_setup_tick != state.last_car_setup_tick:
-            state.last_car_setup_tick = car_setup_tick
-            # print('car setup update count:', car_setup['UpdateCount'])
-            # now you can go to garage, and do some changes with your setup
-            # this line will be printed, only when you change something
-            # and press apply button, but not every 1 sec
-    # note about session info data
-    # you should always check if data exists first
-    # before do something like ir['WeekendInfo']['TeamRacing']
-    # so do like this:
+    print(len(ir['CarIdxTrackSurface']))
+    def SFget():
 
-    #if ir['WeekendInfo']:
-    #    print(ir['WeekendInfo'])
 
-    print(ir['irsdk_TrkLoc'])
+        fuelLevel = round(ir['FuelLevel'],2)
+        fuelLastLap = round(state.previousFuelLevel - fuelLevel,2)
+        airTemp = round(ir['AirTemp'],1)
+        lastLapTime = ir['LapLastLapTime']
+        #min =
+        lastLapTimestr = str(int(lastLapTime // 60)).zfill(2) + ":"\
+                         + str(int(lastLapTime - ((lastLapTime // 60)*60))).zfill(2) + "."\
+                         + str(round(lastLapTime-int(lastLapTime),4))[2:-1]
+        last3LapsTimes = ir['LapLastNLapTime']
+        classPos = ir['PlayerCarClassPosition']
+        raceLap = ir['RaceLaps']
+        windDir = round(ir['WindDir'])
+        windVel = round(ir['WindVel'])
+        sessionTime = ir['SessionTime']
+        sessionTimestr = str(int(sessionTime // 3600)).zfill(2) + ":"\
+                         + str(int((sessionTime - sessionTime //3600 * 3600)//60)).zfill(2) + ":" \
+                         + str(int(sessionTime-((sessionTime//60)*60))).zfill(2)
+        sessionTimeRemain = ir['SessionTimeRemain']
+        sessionTimeRemainstr = str(int(sessionTimeRemain // 3600)).zfill(2) + ":"\
+                         + str(int((sessionTimeRemain - sessionTimeRemain //3600 * 3600)//60)).zfill(2) + ":" \
+                         + str(int(sessionTimeRemain-((sessionTimeRemain//60)*60))).zfill(2)
 
-    # and just as an example
-    # you can send commands to iracing
-    # like switch cameras, rewind in replay mode, send chat and pit commands, etc
-    # check pyirsdk.py library to see what commands are available
-    # https://github.com/kutu/pyirsdk/blob/master/irsdk.py#L134 (class BroadcastMsg)
-    # when you run this script, camera will be switched to P1
-    # and very first camera in list of cameras in iracing
-    # while script is running, change camera by yourself in iracing
-    # and notice how this code changes it back every 1 sec
-    #ir.cam_switch_pos(0, 1)
-    #print(state)
-    #print(ir)
+        print("Fuel remaining " +str(fuelLevel))
+        state.previousFuelLevel = fuelLevel
+        print("Fuel used " + str(fuelLastLap))
+        print("Air temp is " +str(airTemp))
+        #print("Laptime was " + str(lastLapTime))
+        print("Laptime was " + lastLapTimestr)
+        print("Position " + str(classPos))
+        print("Laps completed " + str(lap))
+        print("Race laps completed " + str(raceLap))
+        print("Wind is " + str(windVel) + " " + str(windDir))
+        print("Session time elapsed " + str(sessionTimestr))
+        print("Session time remaining " + str(sessionTimeRemainstr))
+        print(type(last3LapsTimes))
 
-    #print("track lockation is " + str(trk))
-    # print('\rrpm: ', round(rpm), end='', flush=True)
 
-def loop2():
-    # on each tick we freeze buffer with live telemetry
-    # it is optional, but useful if you use vars like CarIdxXXX
-    # this way you will have consistent data from those vars inside one tick
-    # because sometimes while you retrieve one CarIdxXXX variable
-    # another one in next line of code could change
-    # to the next iracing internal tick_count
-    # and you will get incosistent data
-    ir.freeze_var_buffer_latest()
 
     lap = ir['Lap']
-    fuelLevel = round(ir['FuelLevel'],2)
-    fuelLastLap = state.previousFuelLevel - fuelLevel
-    airTemp = round(ir['Airtemp'],1)
-    lastLapTime = round(ir['LapLastLapTime'],2)
 
-    #print(state.lap_counter)
-    #print(lap)
-    #print(ir['SessionFlags'])
-    print(ir['DriverInfo']['Drivers'][0]['IRating'])
-    print(ir['TrackTempCrew'])
     if lap != state.lap_counter:
         state.lap_counter = lap
-        print(lap)
-        print(ir['LapLastLapTime'])
+        time.sleep(0.5)
+        SFget()
 
 
 
@@ -138,16 +120,10 @@ if __name__ == '__main__':
 
             # check if we are connected to iracing
             check_iracing()
-            #a = ir.var_headers_names
-            #for i in a:
-            #    print(i)
-            #for i in ir._var_headers_dict:
-            #    print(i)
-            # print("very good " + str(int(time.time() - start)))
 
             # if we are, then process data
             if state.ir_connected:
-                loop2()
+                loop()
             # sleep for 1 second
             # maximum you can use is 1/60
             # cause iracing updates data with 60 fps
