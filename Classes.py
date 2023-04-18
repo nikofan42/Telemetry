da0dc5c86ff7aaaabc0b9b0a007ba6b196c072d3
niko <blankkeni@gmail.com>
2023-04-18 16:51:42 +0300

    updates

---
+++ Classes.py
@@ -0,0 +1,36 @@
+import time
+
+# this is our State class, with some helpful variables
+
+
+class State:
+
+    ir_connected = False
+    last_car_setup_tick = -1
+    lap_counter = 0
+    previousFuelLevel = 0
+    in_startup = 1
+    pit_flag = 0
+    t = time.localtime()
+    start_time = time.strftime("%H:%M:%S", t)
+    a = 0
+    sleeptime = 0.5
+    idx = 0
+    inPitBox = False
+    onPitRoad = False
+    pitting = False
+    pitLaneTime = 0
+    pitBoxTime = 0
+    pitCounter = 0
+    pitUpdated = False
+    myName = ""
+    competitorAiDataDictionary = {}
+    competitorRaceDataDictionary = {}
+    debug = "debug"
+    sessionID = ""
+
+class Firebase:
+    db = ""
+    config = ""
+    firebase = ""
+
